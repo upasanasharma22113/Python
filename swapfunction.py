8d39bf0ab89c810e9f5074ea630ad60e9b9ad7ec
LAPTOP-77MSQD5K\HP <upasana22113@gmail.com>
2022-10-09 23:12:44 +0530

    swap function learning

---
+++ swapfunction.py
@@ -0,0 +1,13 @@
+from tkinter import Y
+
+
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+x=1 
+y=2
+z=y
+y=x
+x=z
+print("value of x is:" ,x)
+print("value of y is:" ,y)
+
