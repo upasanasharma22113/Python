8f5a89a92ee8d58d266f5515975eca998c50112e
Upasana Sharma <114800357+upasanasharma22113@users.noreply.github.com>
2022-10-28 23:15:40 +0530

    Add files via upload
    learning strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+u= "     HELLO WORLD "
+s= "HELLO"
+P= "WORLD"
+print(len(u))
+print(u[-1])
+print(u[-5:-1])
+print(u[1:5])
+print(u.lower())
+print(u.strip())
+print(u.replace("H","Y"))
+print(u.center(20)) 
+w= s+P
+print(w)
+y= "upas\'ana"
+print(y) 
+
+
