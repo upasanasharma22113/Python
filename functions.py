646fa31331e982fe5140f3fc34fe75e9f3dfe06a
Upasana Sharma <114800357+upasanasharma22113@users.noreply.github.com>
2022-11-11 20:35:59 +0530

    Add files via upload
    practice problems

---
+++ functions.py
@@ -0,0 +1,17 @@
+#HELLO FUNCTION
+def hello():
+    print("hello world")
+    print("hello upasana")
+
+hello()    
+
+def hello_world():
+    print("hello world")
+hello_world()
+
+
+def hello_upasana():
+    print("hello upasana")
+hello_upasana() 
+
+#PARAMETRISED FUNCTION
