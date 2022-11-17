5cfc1c8ec63424176b2dbff11cf1ab39aaa073be
Upasana Sharma <114800357+upasanasharma22113@users.noreply.github.com>
2022-11-17 16:40:24 +0530

    Add files via upload
    addition , subtraction , multiplication functions

---
+++ add.py
@@ -0,0 +1,20 @@
+#ADDITION FUNCTION
+def add(num1, num2):
+    sum = num1+num2
+    print(sum)
+num1=int(input("enter first number: "))  
+num2=int(input("enter second number: "))
+add(num1, num2)
+
+#SUBTRACTION FUNCTION
+def sub(a1, a2):
+    sub= a1-a2
+    print(sub)
+a1=int(input("enter first number: ")) 
+a2=int(input("enter second number: ")) 
+sub(a1,a2)  
+
+#MULTIPLICATION FUNCTION
+def multiply(a1 , a2):
+   return a1*a2
+print(multiply(a1 , a2))   
